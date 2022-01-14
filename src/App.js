6ee5ef034eb2873fee2c6a1956fa99f6b8f4af88
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2022-01-14 15:20:44 +0100

    Testing API functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
+const api = 'https://localhost:44335/'
+
+// Test. This should be its own component.
+
 function App() {
+  const [tournaments, setTournaments] = useState([]);
+
+  async function getTournamentsNear() {
+    let url = api + 'tournaments/near';
+    url += '?latitude=10.000000?longitude=15.000000';
+  
+    const response = await fetch(url);
+    const tournaments = await response.json();
+    
+    setTournaments(currentTournaments => {
+      return tournaments;      
+    })    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header update={getTournamentsNear} />      
     </div>
   );
 }
